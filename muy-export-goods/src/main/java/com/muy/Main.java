059c300ed0a18e05eb0d79b426287dd486c2334b
yanglikai <fjtnylk@126.com>
2017-08-08 21:24:45 +0800

    #1.logback日志改善 #2.dubbo服务日志打印 #3.dubbo服务打包方式改善

--- muy-export-goods/src/main/java/com/muy/Main.java
+++ muy-export-goods/src/main/java/com/muy/Main.java
@@ -1,18 +1,26 @@
 package com.muy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by yanglikai on 2017/7/28.
  */
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     private static final boolean running = true;
 
     public static void main(String[] args) {
         try {
+            log.info("dubbo-goods-provider starting");
+            System.out.println("dubbo-goods-provider starting");
             new ClassPathXmlApplicationContext("classpath*:spring-export-goods.xml").start();
+            log.info("dubbo-goods-provider started");
+            System.out.println("dubbo-goods-provider started");
         } catch (Exception ex) {
-            System.out.println(String.format("dubbo-goods-provider context start error![%s]", ex.getMessage()));
+            log.error("dubbo-goods-provider context start error![%s]", ex.getMessage());
             System.exit(1);
         }
 
@@ -21,7 +29,7 @@ public class Main {
                 try {
                     Main.class.wait();
                 } catch (Throwable e) {
-                    System.out.println(String.format("synchronized error![%s]", e.getMessage()));
+                    log.error("synchronized error![%s]", e.getMessage());
                     System.exit(1);
                 }
             }
--- muy-export-user/src/main/java/com/muy/Main.java
+++ muy-export-user/src/main/java/com/muy/Main.java
@@ -1,18 +1,26 @@
 package com.muy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by yanglikai on 2017/7/26.
  */
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     private static volatile boolean running = true;
 
     public static void main(String[] args) {
         try {
+            log.info("dubbo-user-provider starting");
+            System.out.println("dubbo-user-provider starting");
             new ClassPathXmlApplicationContext("classpath*:spring-export-user.xml").start();
+            log.info("dubbo-user-provider started");
+            System.out.println("dubbo-user-provider started");
         } catch (Exception ex) {
-            System.out.println(String.format("dubbo-user-provider context start error![%s]", ex.getMessage()));
+            log.error("dubbo-user-provider context start error![%s]", ex.getMessage());
             System.exit(1);
         }
 
@@ -21,7 +29,7 @@ public class Main {
                 try {
                     Main.class.wait();
                 } catch (Throwable e) {
-                    System.out.println(String.format("synchronized error![%s]", e.getMessage()));
+                    log.error("synchronized error![%s]", e.getMessage());
                     System.exit(1);
                 }
             }
